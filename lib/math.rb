a9225022cb7e8dea02547abfd162b8d7c762b85b
kvignali <kaitlin@flatironschool.com>
2015-06-02 11:17:03 -0400

    done

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,43 @@
 def addition(num1, num2)
+  num1 + num2
 end
+
+1 + 2
 
 def subtraction(num1, num2)
+  num1 - num2
 end
+
+  2 - 1
 
 def division(num1, num2)
+  num1 / num2
 end
+  
+  6 / 3
 
 def multiplication(num1, num2)
+  num1 * num2
 end
+
+  4 * 5
 
 def modulo(num1, num2)
+  num1 % num2
 end
+
+  6 % 3 
 
 def square_root(num)
+  Math.sqrt(num)
 end
+  
+  Math.sqrt(49)
 
 def order_of_operation(num1, num2, num3, num4)
+  num1 + (( num2 * num3 ) / num4 )  
   #Hint:  __ + (( __ * __ ) / __ )
 end
+  
+  6 + ((2 * 1) / 4)
+
