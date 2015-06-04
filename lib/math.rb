7119d2eb624fd1dd6796d82bfcff45b9652c4d0c
Henry Dotson <henry.d.dotson@gmail.com>
2015-06-03 20:42:45 -0400

    done

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,31 @@
 def addition(num1, num2)
+	sum = num1 + num2 
+	sum
 end
 
 def subtraction(num1, num2)
+	diff = num1 - num2
+	diff
 end
 
 def division(num1, num2)
+	quot = num1/num2
+	quot
 end
 
 def multiplication(num1, num2)
+	prod = num1 * num2
 end
 
 def modulo(num1, num2)
+	mod = num1 % num2
 end
 
 def square_root(num)
+	root = Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  ord = num1 + (( num2 * num3 ) / num4 )
+  ord
 end
